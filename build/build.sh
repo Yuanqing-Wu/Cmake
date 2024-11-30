00804d7a1cc0bc96b7b2ef915e83d4bf8b8822ea
Yuanqing Wu <767296827@qq.com>
2024-11-30 15:46:27 +0800

    add CMAKE_BUILD_TYPE

--- build/build.sh
+++ build/build.sh
@@ -1,2 +1,2 @@
 find . -type f -not -name 'build.sh' -exec rm -f {} +
-cmake -G Ninja -DUSE_LIBRARY=ON -DCMAKE_CXX_COMPILER=clang++ ..
+cmake -G Ninja -DUSE_LIBRARY=ON -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Debug ..
