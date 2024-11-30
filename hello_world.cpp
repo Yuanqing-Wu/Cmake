b369500cd3e9a77c398aa4d731366181506779b5
Yuanqing Wu <767296827@qq.com>
2024-11-30 15:19:22 +0800

    add library

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,16 @@
+
+#include "message.h"
+
 #include <cstdlib>
 #include <iostream>
-#include <string>
 
-std::string say_hello() { return std::string("Hello, CMake world!"); }
 
 int main() {
-    std::cout << say_hello() << std::endl;
+    Message say_hello("Hello, CMake World!");
+    std::cout << say_hello << std::endl;
+
+    Message say_goodbye("Goodbye, CMake World");
+    std::cout << say_goodbye << std::endl;
+    
     return EXIT_SUCCESS;
 }
